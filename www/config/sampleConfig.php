7cf726a07a401eb82249985b34ab30a6091989e9
AhmedMarzouk266 <AhmedMarzouk266@gmail.com>
2018-02-12 11:30:45 +0200

    ADD sampleConfig.php

---
+++ www/config/sampleConfig.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace config;
+/*
+
+class Config {
+
+    public static function getUserData(){
+        // 1C data
+        $userData = array(
+            'user'=>'test',
+            'pass'=>'test'
+        );
+
+        return $userData;
+    }
+}
+
+*/
+
